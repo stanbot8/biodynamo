079af85ba2f5c71a1a16139ccf29c55cb8204cb4
johnpapad24 <92296237+johnpapad24@users.noreply.github.com>
2026-02-10 22:14:48 +0000

    Update ParaView.cmake
    resolve comment

--- cmake/external/ParaView.cmake
+++ cmake/external/ParaView.cmake
@@ -5,9 +5,9 @@ SET(PARAVIEW_SOURCE_DIR "${CMAKE_THIRD_PARTY_DIR}/paraview")
 if(APPLE AND "${DETECTED_ARCH}" STREQUAL "i386")
   # The release of cmake 3.23.0 broke our build of ParaView on MacOSX. 
   # The build was fixed with a reupload and carries the additional tag cm233.
-  SET(PARAVIEW_TAR_FILE paraview_v5.10.0_cm323_osx-15.7-i386_default.tar.gz)
+  SET(PARAVIEW_TAR_FILE paraview_v5.10.0_cm323_${DETECTED_OS_VERS}_default.tar.gz)
 elseif(APPLE AND "${DETECTED_ARCH}" STREQUAL "arm64")
-  SET(PARAVIEW_TAR_FILE paraview_v5.10.0_osx-15.7-arm64_default.tar.gz)
+  SET(PARAVIEW_TAR_FILE paraview_v5.10.0_${DETECTED_OS_VERS}_default.tar.gz)
 else()
   SET(PARAVIEW_TAR_FILE paraview_v5.9.0_${DETECTED_OS_VERS}_default.tar.gz)
 endif()
