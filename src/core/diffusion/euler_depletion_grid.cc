eaf2126ec216cf6a8dd3af5908be6c0c1fde3040
Stanley C. <19547104+stanbot8@users.noreply.github.com>
2026-09-08 20:09:06 -0700

    Use protected member accessors

--- src/core/diffusion/euler_depletion_grid.cc
+++ src/core/diffusion/euler_depletion_grid.cc
@@ -27,7 +27,10 @@ void EulerDepletionGrid::ApplyDepletion(real_t dt) {
   // want to continue to use c1 for the next step. Thus, we swap pointers here
   // (and again after the depletion). This is necessary because ApplyDepletion
   // is called after the diffusion of the EulerGrid (swaps pointer at the end).
-  std::swap(c1_, c2_);
+  SwapBuffers();
+  const size_t nboxes = GetNumBoxes();
+  real_t* c1 = GetConcentrationPtr();
+  real_t* c2 = GetScratchPtr();
 
   for (size_t s = 0; s < binding_substances_.size(); s++) {
     if (binding_coefficients_[s] == 0.0) {
@@ -50,14 +53,14 @@ void EulerDepletionGrid::ApplyDepletion(real_t dt) {
       auto* depleting_concentration =
           rm->GetDiffusionGrid(binding_substances_[s])->GetAllConcentrations();
 #pragma omp parallel for simd
-      for (size_t c = 0; c < total_num_boxes_; c++) {
-        c2_[c] -=
-            c1_[c] * binding_coefficients_[s] * depleting_concentration[c] * dt;
+      for (size_t c = 0; c < nboxes; c++) {
+        c2[c] -=
+            c1[c] * binding_coefficients_[s] * depleting_concentration[c] * dt;
       }
     }
   }
   // See comment above.
-  std::swap(c1_, c2_);
+  SwapBuffers();
 }
 
 void EulerDepletionGrid::DiffuseWithClosedEdge(real_t dt) {
